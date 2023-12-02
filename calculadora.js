26c3b723762601e41b452363da7c6a93f3b45d74
Jessica Cabral <j.p.cursos@gmail.com>
2023-12-02 12:06:33 -0300

    Subindo projeto JS

---
+++ calculadora.js
@@ -0,0 +1,24 @@
+/* 
+1. Crie variáveis que capturem os valores que a usuária inserir e manipule-os nas funções 
+2. Exibir o resultado na tela
+*/
+
+let numA = document.getElementById(numero1Soma)
+let numB = document.getElementById(numero2Soma)
+let resultadoSoma = document.getElementById (numA+numB)
+
+function somar() {
+    const 
+}
+
+function subtrair() {
+
+}
+
+function multiplicar() {
+
+}
+
+function dividir() {
+
+}
---
+++ media.js
@@ -0,0 +1,16 @@
+/*
+  Calcule a média da estudante, em seguida, com base na nota exiba as seguintes 
+  frases:
+    1. Se a nota for 0, mostre na tela: "Infelizmente você zerou a prova :( "
+    2. Se a nota for entre 0.1 e 3, mostre na tela: 
+      "Caramba, deu ruim, você obteve media {nota}! Estude mais e tente novamente!"
+    3. Se a nota for entre 3.1 e 5.9, mostre na tela: 
+      "Você obteve media {nota}! Falta pouco para a média."
+    4. Se a nota for entre 6 e 7, mostre na tela: "Você está na média com {nota}"
+    5. Se a nota for entre 7.1 e 9.9, mostre na tela: "Notão! Sua média é {nota}!"
+    6. Se a nota for entre 10, mostre na tela: 
+      "Hoje é seu aniversário? Pq você ta de parabéns! 10 de média"
+*/
+function calcularMedia() {
+
+}
