0504fe04adfa20f6a0752e022217b2dc6af2c44a
Jessica Cabral <j.pires.cursos@gmail.com>
2023-12-07 16:45:41 -0300

    atualizando scripts.js

--- media.js
+++ media.js
@@ -15,27 +15,35 @@ const btSituacaoAluno = document.getElementById("btSituacaoAluno")
 
 function calcularMedia() {
 
-      let nota1 = parseFloat (document.querySelector("#nota1").value)
-      let nota2 = parseFloat (document.querySelector("#nota2").value)
-      let nota3 = parseFloat (document.querySelector("#nota3").value)
-      let nota4 = parseFloat (document.querySelector("#nota4").value)
+  let nota1 = parseFloat (document.querySelector("#nota1").value)
+  let nota2 = parseFloat (document.querySelector("#nota2").value)
+  let nota3 = parseFloat (document.querySelector("#nota3").value)
+  let nota4 = parseFloat (document.querySelector("#nota4").value)
 
-      let media = (nota1 + nota2 + nota3 + nota4)/4
-      let mensagem = ""
+  let media = (nota1 + nota2 + nota3 + nota4)/4
+  let mensagem = ""
 
-      if (media ===0) {mensagem = (`Sua média é ${media}. Infelizmente você está reprovado`)
-        } else if (media <= 3) {mensagem = (`Caramba, deu ruim, você obteve media ${media}! Estude mais!`)
-        } else if (media < 6) {mensagem = (`Você obteve media ${media}! Faltou pouco para a média. Estude mais e tente novamente!`)
-        } else if (media <= 7) {mensagem = (`Você está na média! Sua nota é ${media}!`)
-        } else if (media < 10) {mensagem = (`Notão! Sua média é ${media}!`)
-        } else if (media === 10) {mensagem = "Aluno nota 10! Parabéns você está aprovado!"
-        } else {mensagem = ("Verifique os dados inseridos")
-      }
-
-
+  if (media ===0) {
+    mensagem = `Reprovado! Você zerou todas as provas. Média: ${media}.`;
+  } else if (media <= 3) {
+    mensagem = `Caramba, deu ruim, você reprovou! Você obteve media ${media}! Estude mais!`;
+  } else if (media < 6) {
+    mensagem = `Você obteve media ${media}! Faltou pouco para a média. Reprovado!`;
+  } else if (media <= 7) {
+    mensagem = `Atenção! Aprovado na média! Média: ${media}!`;
+  } else if (media < 10) {
+    mensagem = `Notão! Parabéns! Você foi aprovado. Média: ${media}!`;
+  } else if (media === 10) {
+    mensagem = "Uau! Aluno nota 10! Parabéns você foi aprovado!";
+  } else {
+    mensagem = "Verifique os dados inseridos";
+  }
+  
   const situacaoAluno = document.createElement ("p")
   const divmedias = document.getElementById("contentCalcMedias")
   divmedias.appendChild(situacaoAluno).innerText= mensagem
+
 }
 
 btSituacaoAluno.addEventListener ('click', calcularMedia)
+
